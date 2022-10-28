f00f1f617dce17dccbbf19346608052611287b8b
jj <jotajota1302@hotmail.com>
2022-10-28 15:44:57 +0200

    COMMIT

---
+++ login/src/edu/es/eoi/LoginServlet.java
@@ -0,0 +1,46 @@
+package edu.es.eoi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/checkuser")
+public class LoginServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		System.out.println(request.getParameter("mail"));
+		System.out.println(request.getParameter("password"));
+		
+		//Desarrollar un LoginService que acceda a BBDD y a traves de un UserDAO compruebe si el password es correcto
+		
+		response.setContentType("text/html");
+	    PrintWriter out = response.getWriter();
+
+	    out.println("<html>");
+	    out.println("<head>");
+	    out.println("<title>Login</title>");
+	    out.println("</head>");
+	    out.println("<body bgcolor=\"white\">");
+	    out.print("You are logged");
+	    out.print("</br>");
+	    out.print("<a href=/login/login.html>Login</a>");
+	    out.println("</body>");
+	    out.println("</html>");
+
+	}
+
+}
---
+++ login/src/edu/es/eoi/entity/User.java
@@ -0,0 +1,12 @@
+package edu.es.eoi.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class User {
+
+	
+	
+}
